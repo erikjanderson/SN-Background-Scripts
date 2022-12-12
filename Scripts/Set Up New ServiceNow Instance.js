15c8c45cd16168af92aa17d630198813cc6c8c1d
Erik <anderson.erik36@gmail.com>
2022-12-12 11:35:04 -0500

    Update Set Up New ServiceNow Instance.js

--- Scripts/Set Up New ServiceNow Instance.js
+++ Scripts/Set Up New ServiceNow Instance.js
@@ -11,10 +11,22 @@ gs.setProperty('glide.ui.session_timeout', 1440);
 gs.setProperty('glide.ui.vcs.collision_avoidance', false);
 
 
+//Create a basic auth profile so i can import all of my existing work
+var githubUserName = '';
+var githubAccessToken = ''
+var grBAC = new GlideRecord('basic_auth_credentials');
+grBAC.initialize();
+grBAC.setDisplayValue('password',githubAccessToken);
+grBAC.setValue('user_name',githubUserName);
+grBAC.setValue('name','My Github Credentials');
+grBAC.setValue('active',true);
+grBAC.insert();
+
+
 //Activate plugins
 var plugins = [];
 plugins.push('com.glide.hub.integrations.enterprise');
-plugins.push('sn_sg_dependencie');
+plugins.push('sn_sg_dependencies');
 var main = new GlideMultiPluginManagerWorker();
 main.setPluginIds(plugins);
 main.setProgressName("Plugin Installer");
