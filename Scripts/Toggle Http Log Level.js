09719634872d248bf20a3ddd3c2127b31415170e
Erik <anderson.erik36@gmail.com>
2023-08-16 10:14:14 -0400

    Update Toggle Http Log Level.js

--- Scripts/Toggle Http Log Level.js
+++ Scripts/Toggle Http Log Level.js
@@ -4,6 +4,9 @@
     This allows you to view things like response bodies in the outbound http request table.
 */
 var overrideStatus = gs.getProperty('glide.outbound_http_log.override') === 'true'
+
+gs.info("Current override status is: " + overrideStatus + '. Changing to ' + !overrideStatus);
+
 gs.setProperty('glide.outbound_http_log.override', !overrideStatus);
 
 if(!overrideStatus){
