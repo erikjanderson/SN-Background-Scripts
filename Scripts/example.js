e8405773cb94c18dfe34dbf841912eda96b7bbb7
Erik <anderson.erik36@gmail.com>
2023-10-19 08:14:14 -0400

    Update example.js

--- Scripts/example.js
+++ Scripts/example.js
@@ -1,1 +1,10 @@
-//Nothing here yet...
+
+
+//Activate plugins
+var plugins = [];
+plugins.push('com.glide.now.platform.encryption');
+var main = new GlideMultiPluginManagerWorker();
+main.setPluginIds(plugins);
+main.setProgressName("Plugin Installer");
+main.setBackground(true);
+main.start();
