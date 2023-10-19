3120efc42c1282ba8b75f2351e52e3930c8ac3d2
Erik <anderson.erik36@gmail.com>
2023-10-19 08:17:14 -0400

    Update example.js

--- Scripts/example.js
+++ Scripts/example.js
@@ -1,7 +1,13 @@
-
+/* 
+    Title: Activate Plugins Via Script
+    Author: Erik Anderson
+    Description: Activate 1 or more plugins using a background script.
+    For bigger Plugins, you can monitor progress in the sys_execution_tracker table.
+*/
 
 //Activate plugins
 var plugins = [];
+//For each plugin, push the ID into the array
 plugins.push('com.glide.now.platform.encryption');
 var main = new GlideMultiPluginManagerWorker();
 main.setPluginIds(plugins);
