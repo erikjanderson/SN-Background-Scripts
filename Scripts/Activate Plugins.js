fc3e0fd9b411b4baa4945691b5c1f3b6fcc8efde
Erik <anderson.erik36@gmail.com>
2023-10-19 09:01:15 -0400

    Update Activate Plugins.js

--- Scripts/Activate Plugins.js
+++ Scripts/Activate Plugins.js
@@ -14,3 +14,13 @@ main.setPluginIds(plugins);
 main.setProgressName("Plugin Installer");
 main.setBackground(true);
 main.start();
+
+
+
+/*
+    Some Useful Plugin IDs:
+        IntegrationHub Enterprise - com.glide.hub.integrations.enterprise
+        Software Asset Management - com.snc.software_asset_management
+        Software Asset Management Foundation - com.snc.sams
+        Column Level Encryption Enterprise - com.glide.now.platform.encryption
+*/
