eecbe9c00acd0cdd5ee637a414d1cf7764d8154f
Erik <anderson.erik36@gmail.com>
2023-10-26 14:19:09 -0400

    Update Reset App Customizations.js

--- Scripts/Reset App Customizations.js
+++ Scripts/Reset App Customizations.js
@@ -5,15 +5,16 @@
                  This will go through all updates made by the application and allow replace on upgrade
 */
 
-var appId = '';
+var appId = gs.getCurrentApplicationId(); //Gets the current application your user session is in (regardless of what app scope this script runs in)
 
-
-var gr = new GlideRecord('sys_update_xml');
-gr.addQuery('application', appId)
-gr.orderBy('name');
-gr.query();
-while (gr.next()) {
-    gs.info(gr.getValue('name') + ' - ' + gr.getValue('target_name'));
-    gr.replace_on_upgrade = true;
-    gr.update();
+if(appId){
+    var gr = new GlideRecord('sys_update_xml');
+    gr.addQuery('application', appId)
+    gr.orderBy('name');
+    gr.query();
+    while (gr.next()) {
+        gs.info(gr.getValue('name') + ' - ' + gr.getValue('target_name'));
+        gr.replace_on_upgrade = true;
+        gr.update();
+    }
 }
