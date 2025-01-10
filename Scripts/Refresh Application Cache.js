9dcbfed082565cc887f37c87e1360cc8df103b14
Erik Anderson <anderson.erik36@gmail.com>
2025-01-10 13:59:28 -0500

    new file:   Scripts/Refresh Application Cache.js

---
+++ Scripts/Refresh Application Cache.js
@@ -0,0 +1,8 @@
+/* 
+    Title: Refresh Application Cache
+    Author: Erik Anderson
+    Description: Sometimes when you uninstall an application from a ServiceNow Instance it will show up as installed still in the application manager and you wont be able to re-install it.
+    Run this script to force the application cache to fully refresh and the issue should go away after a min or two (I think)
+*/
+
+gs.eventQueue("sn_appclient.check.for.update");
