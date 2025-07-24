0e4385ed016e92b503b0e595d96ac6d8900e4b82
Erik Anderson <anderson.erik36@gmail.com>
2025-07-24 16:52:54 -0400

    updated script to prevet the integration from logging out the same recorrd multiple times as well as updated it to present the output string in a easier to read format.

--- Scripts/Deep Search Application.js
+++ Scripts/Deep Search Application.js
@@ -1,7 +1,7 @@
 /* 
-    Title: Deep search application
-    Author: Erik Anderson
-    Description: Searches an applications metadata files and scans each column for a loose match on a search phrase.
+	Title: Deep search application
+	Author: Erik Anderson
+	Description: Searches an applications metadata files and scans each column for a loose match on a search phrase.
 */
 var applicationId = gs.getCurrentApplicationId(); //Gets the current application your user session is in (regardless of what app scope this script runs in)
 //var applicationId = '';
@@ -17,26 +17,31 @@ gs.info("Search Completed found " + resultCounts + " matching records.");
 
 
 function run(scope, search) {
-    search = search.toLowerCase();
-    var gr = new GlideRecord('sys_metadata');
-    gr.addQuery('sys_scope', scope);
-    gr.query();
-    while (gr.next()) {
-        var table = gr.sys_class_name + '';
-        var acutalRecord = getAcutalRecord(gr.getUniqueValue(), table);
-        for (var key in acutalRecord) {
-            if (acutalRecord.getValue(key) && acutalRecord.getValue(key).toLowerCase().indexOf(search) > -1) {
-                resultCounts++;
-                outputString += "\n\n"+ table + ' : ' + gr.getDisplayValue() + ' : ' + gr.getUniqueValue() + '\n' + acutalRecord.getLink();
-            }
-        }
-    }
+	search = search.toLowerCase();
+	var gr = new GlideRecord('sys_metadata');
+	gr.addQuery('sys_scope', scope);
+	gr.query();
+	while (gr.next()) {
+		var foundInRecord = false;
+		var table = gr.sys_class_name + '';
+		var acutalRecord = getAcutalRecord(gr.getUniqueValue(), table);
+		for (var key in acutalRecord) {
+			if (acutalRecord.getValue(key) && acutalRecord.getValue(key).toLowerCase().indexOf(search) > -1) {
+				if (!foundInRecord) {
+					resultCounts++;
+					outputString += "\n\n" + table + " :: " +gr.getDisplayValue() + '\r\t' + gr.getUniqueValue() + '\n\t' + acutalRecord.getLink();
+					foundInRecord = true;
+				}
+				outputString += "\n\t" + key;
+			}
+		}
+	}
 }
 
 //Gets the glide record for a specific table vs the sys_metadata table;
 function getAcutalRecord(sysId, table) {
-    var gr = new GlideRecord(table);
-    if (gr.get(sysId)) {
-        return gr;
-    }
+	var gr = new GlideRecord(table);
+	if (gr.get(sysId)) {
+		return gr;
+	}
 }
