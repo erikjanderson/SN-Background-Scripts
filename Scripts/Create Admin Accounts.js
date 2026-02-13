f01007a437133f2d132d131467bf1fea3be33a9e
Erik Anderson <anderson.erik36@gmail.com>
2026-02-13 12:06:23 -0500

    updated script to be a bit easier to create accounts in bulk by allowing you to derive the name from an email address automatically so potentially you can just list an email and the script will do the rest..

--- Scripts/Create Admin Accounts.js
+++ Scripts/Create Admin Accounts.js
@@ -6,19 +6,20 @@
 */
 
 var accountInfo = [
-    {'user_name': 'john.doe', 'first_name': 'John', 'last_name': 'Doe', 'email': "john.doe@example.com"},
+    { 'user_name': 'john.doe', 'first_name': 'John', 'last_name': 'Doe', 'email': "john.doe@example.com" },
 ];
 var reportString = '';
 var instanceURL = gs.getProperty('glide.servlet.uri');
-for(var i = 0; i < accountInfo.length; i++){
+for (var i = 0; i < accountInfo.length; i++) {
+    generateMissingAccountDetailsFromEmail(accountInfo[i]);
     var returnObj = createAccount(accountInfo[i]);
     returnObj.randomPassword = updateAccountPassword(accountInfo[i]);
-    if(returnObj && returnObj.userId){
+    if (returnObj && returnObj.userId) {
         addRoleToUser(returnObj.userId, 'admin');
         addRoleToUser(returnObj.userId, 'security_admin');
     }
     reportString += '\n' + instanceURL + '\n' + accountInfo[i].user_name;
-    if(returnObj.randomPassword){
+    if (returnObj.randomPassword) {
         reportString += '\n' + returnObj.randomPassword;
     }
 }
@@ -27,16 +28,16 @@ gs.info(reportString);
 
 
 
-function addRoleToUser(userId, roleName){
+function addRoleToUser(userId, roleName) {
     var roleId = getRole(roleName);
-    if(!roleId || !userId){
+    if (!roleId || !userId) {
         return;
     }
     var rolesGr = new GlideRecord('sys_user_has_role');
     rolesGr.addQuery('role', roleId);
     rolesGr.addQuery('user', userId);
     rolesGr.query();
-    if(!rolesGr.next()){
+    if (!rolesGr.next()) {
         rolesGr.initialize();
         rolesGr.user = userId;
         rolesGr.role = roleId;
@@ -44,42 +45,73 @@ function addRoleToUser(userId, roleName){
     }
 }
 
-function getRole(roleName){
+function getRole(roleName) {
     var rGr = new GlideRecord('sys_user_role');
-    if(rGr.get('name', roleName)){
+    if (rGr.get('name', roleName)) {
         return rGr.getUniqueValue();
     }
 }
 
-function createAccount(userObj){
+function createAccount(userObj) {
     var returnObj = {
         userId: '',
-        randomPassword:''
+        randomPassword: ''
     }
     var userGr = new GlideRecord('sys_user');
-    if(userGr.get('user_name', userObj.user_name)){
+    if (userGr.get('user_name', userObj.user_name)) {
         returnObj.userId = userGr.getUniqueValue();
-    }else{
+    } else {
         //returnObj.randomPassword = GlideSecureRandomUtil.getSecureRandomString(12);
         userGr.initialize();
         var keys = Object.keys(userObj);
-        for(var i = 0; i < keys.length; i++){
+        for (var i = 0; i < keys.length; i++) {
             userGr.setDisplayValue(keys[i], userObj[keys[i]]);
         }
         // userGr.setDisplayValue('user_password', returnObj.randomPassword);
         // userGr.setDisplayValue('password_needs_reset', true);
         returnObj.userId = userGr.insert();
     }
-    return returnObj;    
+    return returnObj;
 }
 
-function updateAccountPassword(userObj){
+function updateAccountPassword(userObj) {
     var randomPassword = SNC.PasswordPolicyEvaluator.generateUserPassword(userObj.user_name);
     var userGr = new GlideRecord('sys_user');
-    if(userGr.get("user_name", userObj.user_name)){
+    if (userGr.get("user_name", userObj.user_name)) {
         userGr.setDisplayValue('user_password', randomPassword);
         userGr.setDisplayValue('password_needs_reset', true);
         userGr.update()
     }
     return randomPassword;
+}
+
+function generateMissingAccountDetailsFromEmail(accountInfo) {
+    var email = accountInfo.email;
+    if (!email || typeof email !== 'string' || !email.includes('@')) {
+        return null;
+    }
+    // Get everything before the @
+    var username = email.split('@')[0];
+    // Split on dots
+    var parts = username.split('.').filter(Boolean);
+    if (parts.length === 0) {
+        return null;
+    }
+    // First name = first segment
+    var firstNameRaw = parts[0];
+    // Last name = last segment
+    var lastNameRaw = parts.length > 1 ? parts[parts.length - 1] : '';
+    // Capitalization helper
+    function capitalize(name) {
+        return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    }
+    if(gs.nil(accountInfo.user_name)){
+        accountInfo.user_name = username;
+    }
+    if(gs.nil(accountInfo.first_name)){
+        accountInfo.first_name = capitalize(firstNameRaw);
+    }
+    if(gs.nil(accountInfo.last_name)){
+        accountInfo.last_name = lastNameRaw ? capitalize(lastNameRaw) : '';
+    }
 }
