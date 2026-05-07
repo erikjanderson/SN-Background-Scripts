7f6bf8fef8ba33faa1a184ed2d5e105a80a5968d
Erik Anderson <anderson.erik36@gmail.com>
2026-05-07 08:20:53 -0400

    modified:   Scripts/Toggle Http Log Level.js

--- Scripts/Toggle Http Log Level.js
+++ Scripts/Toggle Http Log Level.js
@@ -3,14 +3,19 @@
     Description: Toggles the http log levels on the ServiceNow instance. 
     This allows you to view things like response bodies in the outbound http request table.
 */
-var overrideStatus = gs.getProperty('glide.outbound_http_log.override') === 'true'
 
-gs.info("Current override status is: " + overrideStatus + '. Changing to ' + !overrideStatus);
+var overrideStatus = gs.getProperty("glide.outbound_http_log.override") === "true";
 
-gs.setProperty('glide.outbound_http_log.override', !overrideStatus);
+gs.info("Current override status is: " + overrideStatus + ". Changing to " + !overrideStatus);
 
-if(!overrideStatus){
-    gs.setProperty('glide.outbound_http_log.override.level', 'ALL');
-}else{
-    gs.setProperty('glide.outbound_http_log.override.level', '');
+gs.setProperty("glide.outbound_http_log.override", !overrideStatus);
+
+if (!overrideStatus) {
+	gs.setProperty("glide.rest.outbound.debug", "true");
+	gs.setProperty("glide.outbound_http_log.override.level", "ALL");
+	gs.setProperty("glide.outbound_http.content.max_limit", 1000);
+} else {
+	gs.setProperty("glide.rest.outbound.debug", "false");
+	gs.setProperty("glide.outbound_http_log.override.level", "");
+	gs.setProperty("glide.outbound_http.content.max_limit", 100);
 }
